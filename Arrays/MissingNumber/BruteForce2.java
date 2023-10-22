6aa7efbd3adbb0fa476aac23a15768deae9a9b89
AlishaKausheen <alishakausheen@gmail.com>
2023-10-21 20:23:05 -0700

    better

---
+++ Arrays/MissingNumber/BruteForce2.java
@@ -0,0 +1,15 @@
+package Arrays.MissingNumber;
+
+public class BruteForce2 {
+    public static void main(String args[]) {
+        int[] arr = {1, 2, 4, 5};
+        int[] hash = new int[arr.length + 1];
+        for(int i=0;i< arr.length-1;i++){
+            hash[arr[i]]++;
+        }
+        for(int i=1;i<= arr.length;i++){
+            if(hash[i]==0)
+                System.out.print(i+" is missing");
+        }
+    }
+}
