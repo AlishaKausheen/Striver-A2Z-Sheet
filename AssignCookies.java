1a899b99eaf898cf364708ba63f7efb8a3370896
AlishaKausheen <alishakausheen@gmail.com>
2024-05-30 13:19:21 +0530

    assign cookies

---
+++ AssignCookies.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class AssignCookies {
+    public static void main(String[] args) {
+        int[] g = {1,5,3,3,4};
+        int[] s = {4,2,1,2,1,3};
+        System.out.println(maxAssign(g,s));
+
+    }
+    static int maxAssign(int[] g, int[] s){
+        Arrays.sort(s);
+        Arrays.sort(g);
+        int l=0, r=0;
+        while (l<s.length){
+            if(g[r]<=s[l]){
+                r=r+1;
+            }
+            l=l+1;
+        }
+        return r;
+    }
+}
