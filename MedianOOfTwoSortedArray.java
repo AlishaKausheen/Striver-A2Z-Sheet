b0ac3b87c2782aada8dd52ccbd7fb98af109ed2d
AlishaKausheen <alishakausheen@gmail.com>
2024-06-12 17:54:30 +0530

    better approach

--- MedianOOfTwoSortedArray.java
+++ MedianOOfTwoSortedArray.java
@@ -5,6 +5,7 @@ public class MedianOOfTwoSortedArray {
         int[] arr1={1,3,4,7,10,12};
         int[] arr2 = {2,3,6,15};
         System.out.println(brute(arr1,arr2));
+        System.out.println(better(arr1,arr2));
     }
 
     static double brute(int[] arr1, int[] arr2){
@@ -37,5 +38,50 @@ public class MedianOOfTwoSortedArray {
         }
  return median;
     }
+static double better(int[] arr1, int[] arr2){
+        double median =0;
 
+        int n= arr1.length+ arr2.length;
+        int i=0,j=0;
+        int count=0;
+        int index1=n/2,index2=n/2-1;
+        int ind1ele=-1,ind2ele=-1;
+        while (i<arr1.length&& j<arr2.length){
+            if (arr1[i]<arr2[j]){
+                if (count==index1)
+                    ind1ele=arr1[i];
+                if (count==index2)
+                    ind2ele=arr1[i];
+                count++;
+                i++;
+            }else {
+                if (count==index1)
+                    ind1ele=arr2[j];
+                if (count==index2)
+                    ind2ele=arr2[j];
+                count++;
+                j++;
+            }
+        }
+        while (i<arr1.length){
+            if (count==index1) ind1ele=arr1[i];
+            if (count==index2) ind2ele=arr1[i];
+            i++;
+            count++;
+        }
+    while (j<arr2.length){
+        if (count==index1) ind1ele=arr2[j];
+        if (count==index2) ind2ele=arr2[j];
+        j++;
+        count++;
+    }
+    if (n%2==1){
+        median= ind2ele;
+    }else {
+        median = (double)(ind1ele+ind2ele)/2.0;
+
+    }
+
+        return median;
+}
 }
