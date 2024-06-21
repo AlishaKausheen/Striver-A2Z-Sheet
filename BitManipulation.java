965eee7cff7bd0f4ec5a9dba792a498f0261651f
AlishaKausheen <alishakausheen@gmail.com>
2024-06-21 13:21:00 +0530

    get set clear bit

--- BitManipulation.java
+++ BitManipulation.java
@@ -2,5 +2,21 @@ public class BitManipulation {
     public static void main(String[] args) {
         int num=70,i=3;
     }
-    
+   //bits starts from 1
+    static int getbit(int num, int i){
+
+
+        int n= (num>>(i-1))&1;
+        return n;
+    }
+    static int setbit(int num,int i){
+        int k=1<<(i-1);
+        int d=num|k;
+        return d;
+    }
+    static int clearbit(int num, int i){
+        int k=1<<(i-1);
+        int f=(~k&num);
+        return f;
+    }
 }
