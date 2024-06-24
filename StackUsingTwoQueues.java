407971ff40e850e3310541d42e391c6f171f3a8c
AlishaKausheen <alishakausheen@gmail.com>
2024-06-24 13:38:02 +0530

    implement stack using two queues

---
+++ StackUsingTwoQueues.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsingTwoQueues {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public StackUsingTwoQueues() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+
+    }
+
+    public void push(int x) {
+        while(!q1.isEmpty()){
+            q2.offer(q1.poll());
+        }
+        q1.offer(x);
+        while(!q2.isEmpty()){
+            q1.offer(q2.poll());
+        }
+
+    }
+
+    public int pop() {
+        if(q1.isEmpty()) return -1;
+        return q1.poll();
+    }
+
+    public int top() {
+        if(q1.isEmpty()) return -1;
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
