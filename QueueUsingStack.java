fd3d18e7db5178f3c61bfb8ab3a421bace412c26
AlishaKausheen <alishakausheen@gmail.com>
2024-06-24 14:03:12 +0530

    implement queues using stack

--- QueueUsingStack.java
+++ QueueUsingStack.java
@@ -1,2 +1,43 @@
+import java.util.Stack;
+
 public class QueueUsingStack {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+    QueueUsingStack(){
+        s1= new Stack<>();
+        s2= new Stack<>();
+    }
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+
+            while (!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            int ans = s2.pop();
+            while (!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+
+       return ans;
+    }
+
+    public int peek() {
+
+        while (!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        int ans = s2.peek();
+        while (!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
+
+        return ans;
+    }
+
+    public boolean empty() {
+       return s1.isEmpty()&&s2.isEmpty();
+    }
 }
