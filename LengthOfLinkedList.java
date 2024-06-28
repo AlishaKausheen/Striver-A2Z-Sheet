b0b2aa05b6a6e84fc8c170e18569f39584f14be9
AlishaKausheen <alishakausheen@gmail.com>
2024-06-28 16:13:21 +0530

    length of linkedlist

--- LengthOfLinkedList.java
+++ LengthOfLinkedList.java
@@ -1,2 +1,25 @@
 public class LengthOfLinkedList {
+    class Node{
+        int data;
+        Node next;
+        Node(int a){  data = a; next = null; }
+    }
+
+    class Solution
+    {
+        //Function to count nodes of a linked list.
+        public static int getCount(Node head)
+        {
+
+            //Code here
+            int count =0;
+            Node temp=head;
+            while(temp!=null){
+                temp =temp.next;
+                count++;
+            }
+            return count;
+        }
+    }
+
 }
